bf0668ccc3a5c7fe2d7fef81d61cb118e9cf430a
Rylie Nelson <ryliemn@gmail.com>
2018-03-20 21:10:52 -0700

    add help command to addartist

--- commands/addartist.js
+++ commands/addartist.js
@@ -11,15 +11,20 @@ async function addartist(message) {
   db.connect();
   const artistName = message.split(' ').splice(1).join(' ');
   let response = null;
-  try {
-    const result = await db.query(
-      'SELECT * FROM rateify.add_artist($1)',
-      [artistName]
-    );
-    response = `I made the following new artist record:
-      ${result.rows[0].id} \t\t ${result.rows[0].name}`;
-  } catch (err) {
-    response = err.stack;
+  if (artistName !== 'help') {
+    try {
+      const result = await db.query(
+        'SELECT * FROM rateify.add_artist($1)',
+        [artistName]
+      );
+      response = `I made the following new artist record:
+        ${result.rows[0].id} \t\t ${result.rows[0].name}`;
+    } catch (err) {
+      response = err.stack;
+    }
+  } else {
+    response = `Usage:
+      .addartist [Artist Name]`;
   }
   db.end();
   return response;
